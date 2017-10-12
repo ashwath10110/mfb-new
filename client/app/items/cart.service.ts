73b5df06079c7d9e8647de15add049329829c37c
gs-ashwath <abharadwaj@gainsight.com>
2017-10-13 01:27:11 +0530

    cart adding corrected.

--- client/app/items/cart.service.ts
+++ client/app/items/cart.service.ts
@@ -10,49 +10,46 @@ export class CartService {
   cartTotal: number = 0
 
   private productAddedSource = new Subject<any>()
-
-
   productAdded$ = this.productAddedSource.asObservable()
 
   constructor() { }
 
-  addProductToCart(product){
-    let exists = false
-    let parsedPrice = parseFloat(product.price.replace(/\./g, '').replace(',', '.'))
-    this.cartTotal += parsedPrice
+  addProductToCart(product) {
+    let exists = false;
+    let parsedPrice = parseFloat(product.price.replace(/\./g, '').replace(',', '.'));
+    this.cartTotal += parsedPrice;
     //Search this product on the cart and increment the quantity
-    this.products = this.products.map(_product => {
-      if(_product.product.id == product.id){
-        _product.quantity++
-        exists = true
+
+    for (var i = 0; i < this.products.length; i++) {
+      if (this.products[i].product._id == product._id) {
+        this.products[i].quantity++;
+        exists = true;
       }
-      return _product
-    })
+    }
     //Add a new product to the cart if it's a new product
-    if(!exists){
+    if (!exists) {
       product.parsedPrice = parsedPrice
       this.products.push({
-        product:product,
-        quantity:1
+        product: product,
+        quantity: 1
       })
     }
-
     this.productAddedSource.next({ products: this.products, cartTotal: this.cartTotal })
   }
 
-  deleteProductFromCart(product){
-    this.products = this.products.filter(_product => {
-      if(_product.product.id == product.id){
-        this.cartTotal -= _product.product.parsedPrice*_product.quantity
-        return false
+  deleteProductFromCart(product) {
+    debugger;
+    for (var i = 0; i < this.products.length; i++) {
+      if (this.products[i].product._id == product._id) {
+        this.cartTotal -= this.products[i].product.parsedPrice * this.products[i].quantity;
+        this.products.splice(i, 1);
+        break;
       }
-      return true
-     })
+    }
     this.productAddedSource.next({ products: this.products, cartTotal: this.cartTotal })
   }
 
-
-  flushCart(){
+  flushCart() {
     this.products = []
     this.cartTotal = 0
     this.productAddedSource.next({ products: this.products, cartTotal: this.cartTotal })
