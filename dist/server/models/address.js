b279db9d79396088bc4371bbbab1d4b1126d7278
gs-ashwath <abharadwaj@gainsight.com>
2017-10-15 20:26:43 +0530

    asdf

--- dist/server/models/address.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-var addressSchema = new mongoose.Schema({
-    name: String
-});
-var Address = mongoose.model('Address', addressSchema);
-exports.default = Address;
-//# sourceMappingURL=address.js.map
