fc4d3306d77d8ce7bacefb6a662e225f3d5118b3
gs-ashwath <abharadwaj@gainsight.com>
2017-10-28 17:36:41 +0530

    deleted success component

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -3,7 +3,7 @@ import * as jwt from 'jsonwebtoken';
 
 import User from '../models/user';
 import BaseCtrl from './base';
-import * as sha512 from 'js-sha512';
+// import * as sha512 from 'js-sha512';
 import * as path from 'path';
 
 export default class UserCtrl extends BaseCtrl {
@@ -23,8 +23,8 @@ export default class UserCtrl extends BaseCtrl {
   createHash = (req, res) => {
     var salt = 'eCwWELxi';
     var hash = {};
-    var hash = sha512(req.body.preHashString + salt);
-    console.log(hash);
+    // var hash = sha512(req.body.preHashString + salt);
+    // console.log(hash);
     res.send({success : true, hash: hash});
   }
 
